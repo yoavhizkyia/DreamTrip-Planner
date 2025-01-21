2cca4964fa22c9914063640252c96e64dbc4fc64
yoavhizkyia <yoavhizkyia@gmail.com>
2025-01-21 21:11:04 +0200

    init example with react-hook-form and adding mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
 
 const App = () => {
+  const {handleSubmit} = useForm({
+    defaultValues: {
+      userName: null,
+      password: null 
+    },
+    
+  })
 
 return <>
-  Hello Yasmin!
+  <form onSubmit={handleSubmit((data) => console.log({data}))}>
+    <input type="text" name="userName" placeholder="User Name" />
+    <input type="password" name="password" placeholder="Password" />
+    <button type="submit">Submit</button>
+  </form>
     </>
 }
 
