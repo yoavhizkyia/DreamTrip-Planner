7d917f76c79cd76ffe2816a8bb07d1c378193ce5
yoavhizkyia <yoavhizkyia@gmail.com>
2025-01-31 16:22:35 +0200

    feat(index.css): update theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from 'tailwindcss'
 
 export default {
-    // darkMode: ["class"],
+    darkMode: ["class"],
     content:["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
     theme: {
     	extend: {
